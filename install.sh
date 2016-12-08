45b83c2980212306b5b973ebc48ba3d360f36870
rst0git <rstoyanov1@gmail.com>
2016-12-08 23:38:12 +0000

    Added update and commit

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp ./config.fish ~/.config/fish/config.fish
+cp ./.liquidpromptrc.fish ~/.config/fish/.liquidpromptrc.fish
+cp ./.liquidpromptrc.fish ~/.config/fish/.liquid.theme
---
+++ update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp ~/.config/fish/config.fish ./
+cp ~/.config/fish/.liquidpromptrc.fish ./
+cp ~/.config/fish/.liquid.theme ./
