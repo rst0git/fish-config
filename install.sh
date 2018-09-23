d2299d649a5261294512683e0c9604e4e12ffd98
Radostin Stoyanov <rstoyanov1@gmail.com>
2018-09-23 15:58:29 +0100

    install: Create ~/.config/fish
    Signed-off-by: Radostin Stoyanov <rstoyanov1@gmail.com>

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir -p ~/.config/fish/
 cp ./config.fish ~/.config/fish/config.fish
 cp ./.liquidpromptrc.fish ~/.config/fish/.liquidpromptrc.fish
 cp ./.liquidpromptrc.fish ~/.config/fish/.liquid.theme
